71ab9658fd5f506b8c270614e0ae01e7d1711235
vabole <vabole@gmail.com>
2015-10-01 18:22:36 +0300

    Implemetation of a class.

---
+++ ComplexNumbers/Complex.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ComplexNumbers
+{
+    public class Complex
+    {
+
+        #region Конструкторы
+
+        public Complex(double re = 0, double im = 0)
+        {
+            this.Re = re;
+            this.Im = im;
+        }
+
+        #endregion
+
+        #region Свойства
+
+        /// <summary>
+        /// Вещественная часть числа
+        /// </summary>
+        public double Re { get; set; }
+
+        /// <summary>
+        /// Мнимая часть числа
+        /// </summary>
+        public double Im { get; set; }
+
+        /// <summary>
+        /// Модуль комплексного числа в полярных координатах
+        /// </summary>
+        public double R
+        {
+            get { return Math.Sqrt(Re*Re + Im*Im); }
+
+            set
+            {
+                var phi = Phi;
+                Re = PolarToRectangularMagnitude(value, phi);
+                Im = PolarToRectangularAngle(value, phi);
+            }
+        }
+        
+        /// <summary>
+        /// Аргумент комплексного числа в полярных координатах
+        /// </summary>
+        public double Phi
+        {
+            get
+            {
+                return Re == 0
+                    ? Math.PI /2 * Math.Sign(Im)
+                    : Math.Atan(Im/Re) + Math.PI*Math.Sign(Re);
+            }
+            set
+            {
+                var magnitude = R;
+                Re = PolarToRectangularMagnitude(magnitude, value);
+                Im = PolarToRectangularAngle(magnitude, value);
+            }
+        }
+
+        #endregion
+
+        #region Переопределение операций
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.Re + b.Re, a.Im + b.Im);
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.Re - b.Re, a.Im - b.Im);
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a.Re * b.Re - a.Im*b.Im, a.Im *b.Re + a.Re*b.Im);
+        }
+
+        public static Complex operator /(Complex a, Complex b)
+        {
+            double denominator = b.Re*b.Re + b.Im*b.Im;
+            return new Complex( (a.Re*b.Re + a.Im*b.Im)/ denominator, (a.Im*b.Re - a.Re * b.Im)/ denominator );
+        }
+        #endregion
+
+        #region Переопределение операций сравнения
+
+        public static bool operator ==(Complex a, Complex b)
+        {
+            if (ReferenceEquals(a, null))
+            {
+                return ReferenceEquals(b, null);
+            }
+            else
+            {
+                return a.Equals(b);
+            }
+        }
+
+        public static bool operator !=(Complex a, Complex b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return Re.GetHashCode() ^ Im.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var complex = obj as Complex;
+            if ((object) complex == null)
+            {
+                return false;
+            }
+            return (Re == complex.Re) && (Im == complex.Im);
+        }
+
+        #endregion
+
+        #region Операторы приведения типов
+
+        /// <summary>
+        /// Явное преобразование в вещественное число
+        /// </summary>
+        /// <param name="a"></param>
+        public static explicit operator double(Complex a)
+        {
+            return a.Re;
+        }
+
+        /// <summary>
+        /// Неявное преобразование числа double в Complex
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns>Комплексное число с мнимой частью равной нулю</returns>
+        public static implicit operator Complex(double a)
+        {
+            return new Complex(a);
+        }
+
+        #endregion
+
+        #region Методы 
+        /// <summary>
+        /// Вычисляет вещественную часть комплексного числа заданного в полярных координатах
+        /// </summary>
+        /// <param name="r">Модуль</param> 
+        /// <param name="phi">Угол</param>
+        /// <returns>Вещественная часть комплексного числа</returns>
+        private static double PolarToRectangularMagnitude(double r, double phi)
+        {
+            return r*Math.Cos(phi);
+        }
+
+        /// <summary>
+        /// Вычисляет мнимую часть комплекного числа заданного в полярных координатах
+        /// </summary>
+        /// <param name="r">Модуль</param>
+        /// <param name="phi">Угол</param>
+        /// <returns>Мнимая часть комплексного числа</returns>
+        private static double PolarToRectangularAngle(double r, double phi)
+        {
+            return r*Math.Sin(phi);
+        }
+
+        public static Complex CreateByPolar(double r=0, double phi = 0)
+        {
+            return new Complex( PolarToRectangularMagnitude(r, phi), PolarToRectangularAngle(r, phi) );
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}{1}{2}", 
+                Re == 0 ? "" : Re.ToString("g4"),
+                Re != 0 && Im > 0 ? "+" : "",
+                Im != 0 ? Im.ToString("g4") + "i" : "" );
+        }
+        #endregion
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ComplexNumbers;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Complex(1,1);
+            // Вызов ToString() для числа 1+1i
+            Console.WriteLine("Создали новый экземпляр комплексного числа: c1 = {0}\n", c1);
+            // Отображение вещественной и мнимой частей
+            Console.WriteLine("Отображение свойств: с1.Re = {0}, c1.Im = {1}",c1.Re, c1.Im);
+            Console.WriteLine("Вычислимые свойства: с1.R = {0}, c1.Phi = {1}\n",c1.R, c1.Phi);
+
+            c1.Re = 1;
+            c1.Im = -5;
+            var c2 = Complex.CreateByPolar(Math.Sqrt(2), Math.PI/4);
+            Console.WriteLine("Создали новый экземпляр комплексного числа c2, используя полярные координаты:");
+            Console.WriteLine("c2( sqrt(2), PI/4 ) = {0}\n",c2);
+
+            var c3 = new Complex(im:-10);
+            Console.WriteLine("Создали новый экземпляр комплексного числа c3 = {0}\n",c3);
+
+            Console.WriteLine("Арифметические операции:");
+            var c4 = new Complex(2,0);
+            var c5 = new Complex(2.0);
+            var c6 = new Complex(3, 7);
+            var c7 = new Complex(6, -9.5);
+            var c8 = new Complex(im: 4);
+
+            Console.WriteLine("({0}) * ({1}) = {2}", c4, c5, c4 * c5 );
+            Console.WriteLine("({0}) * ({1}) = {2}", c6, c7, c6 * c7);
+            Console.WriteLine("({0}) * ({1}) = {2}\n", c3, c8, c3 * c8);
+
+            Console.WriteLine("({0}) + ({1}) = {2}", c4, c5, c4 + c5);
+            Console.WriteLine("({0}) + ({1}) = {2}", c6, c7, c6 + c7);
+            Console.WriteLine("({0}) + ({1}) = {2}\n", c3, c8, c3 + c8);
+
+            Console.WriteLine("({0}) - ({1}) = {2}", c4, c5, c4 - c5);
+            Console.WriteLine("({0}) - ({1}) = {2}", c6, c7, c6 - c7);
+            Console.WriteLine("({0}) - ({1}) = {2}\n", c3, c8, c3 - c8);
+
+            Console.WriteLine("({0}) / ({1}) = {2}", c4, c5, c4 / c5);
+            Console.WriteLine("({0}) / ({1}) = {2}", c6, c7, c6 / c7);
+            Console.WriteLine("({0}) / ({1}) = {2}\n", c3, c8, c3 / c8);
+
+            Console.WriteLine("Операции сравнения:");
+            var c9 = new Complex(2.8,3);
+            var c10 = new Complex(2.8,3);
+            var c11 = new Complex(4,1);
+
+            Console.WriteLine("({0}) == ({1}) => {2}", c9, c10, c9 == c10 );
+            Console.WriteLine("({0}) == ({1}) => {2}\n", c9, c11, c9 == c11);
+
+            Console.WriteLine("({0}) != ({1}) => {2}", c9, c10, c9 != c10);
+            Console.WriteLine("({0}) != ({1}) => {2}\n", c9, c11, c9 != c11);
+
+            Console.WriteLine("Явное преобразование в вещественное число:");
+            Console.WriteLine("{0} => {1}\n", c9, (double) c9);
+
+            Console.WriteLine("Неявное преобразование из вещественного числа");
+            Console.WriteLine("({0}) + {1} = {2}", c11,2,c11 + 2);
+
+            Console.ReadKey();
+        }
+    }
+}
